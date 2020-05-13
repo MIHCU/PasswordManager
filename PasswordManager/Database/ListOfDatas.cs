59cdc598be3d9bc9a4d69a7cd3a77610026308c4
Paweł Kowalski <paw.kowalski@protonmail.com>
2020-05-13 19:17:09 +0200

    added database connection

---
+++ PasswordManager/Database/ListOfDatas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager
+{
+    class ListOfDatas
+    {
+        List<Data> listOfDatas;
+
+        public ListOfDatas()
+        {
+            listOfDatas = new List<Data>();
+        }
+
+        public void Add(Data data)
+        {
+            listOfDatas.Add(data);
+        }
+
+        public Data Give(int index)
+        {
+            return listOfDatas[index];
+        }
+
+        public int Size()
+        {
+            return listOfDatas.Count;
+        }
+
+        public void Delete(Data dataToRemove)
+        {
+            bool found = false;
+            int i = 0;
+            while(found != true)
+            {
+                if (listOfDatas.ElementAt(i).Equals(dataToRemove))
+                {
+                    listOfDatas.RemoveAt(i);
+                    found = true;
+                }
+                i++;
+            }
+        }
+    }
+}
---
+++ PasswordManager/DatabaseWrap.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace PasswordManager
+{
+    internal class DatabaseWrap
+    {
+        private SqlConnection connection = null;
+        private SqlCommand query = null;
+
+        private DatabaseWrap()
+        {
+            ConnectToDatabase();
+        }
+
+        public void AddUser(User newUser)
+        {
+            string query;
+        }
+
+        public bool VerifyUser(User user)
+        {
+            return false;
+        }
+
+        private Data Encrypt(Data dataToEncrypt)
+        {
+            //TODO
+        }
+
+        private Data Decrypt(Data dataToDecrypt)
+        {
+            //TODO
+        }
+
+        private void ConnectToDatabase()
+        {
+            connection = new SqlConnection("Data Source=(local);Initial Catalog=Northwind;Integrated Security=SSPI");
+            connection.Open();
+        }
+
+        private void CloseConnection()
+        {
+            connection.Close();
+            connection.Dispose();
+        }
+
+        private SqlDataReader SendQuery(string queryText)
+        {
+            query = new SqlCommand(queryText, connection);
+            return query.ExecuteReader();
+        }
+
+        public void AddData(Data newData)
+        {
+            string query;
+        }
+
+        public void ModifyData(int id)
+        {
+            string query;
+        }
+
+        public void DeleteData(int id)
+        {
+            string query;
+        }
+
+        public ListOfDatas ShowData()
+        {
+            ListOfDatas list = new ListOfDatas();
+            string query = "SELECT login, pass, tag, notes FROM dbo.data;";
+            SqlDataReader response = SendQuery(query);
+            while (response.HasRows)
+            {
+                list.Add(new Data(response.GetString(0), response.GetString(1), response.GetString(2), response.GetString(3)));
+                response.NextResult();
+            }
+            response.Close();
+            return list;
+        }
+
+        public ListOfDatas SearchData(String searchedTag)
+        {
+            string query = "SELECT login, pass, tag, notes FROM dbo.data WHERE tag ='"+ searchedTag +"';";
+            ListOfDatas list = new ListOfDatas();
+            SqlDataReader response = SendQuery(query);
+            while (response.HasRows)
+            {
+                list.Add(new Data(response.GetString(0), response.GetString(1), response.GetString(2), response.GetString(3)));
+                response.NextResult();
+            }
+            response.Close();
+            return list;
+        }
+    }
+}
